7c87a69b3e6c1fc89c764cf93fbbdc76a8335d37
Vincenzo <123869730+carvso@users.noreply.github.com>
2023-10-09 19:17:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,8 @@
 
 import * as THREE from 'https://unpkg.com/three@0.139.0'
 // import MouseMeshInteraction from './three_mmi.js'
-import {OrbitControls} from 'https://unpkg.com/three@0.139.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.139.0/examples/jsm/controls/OrbitControls.js';
+
 import { TWEEN } from 'https://unpkg.com/three@0.139.0/examples/jsm/libs/tween.module.min.js';
 import {GLTFLoader} from 'https://unpkg.com/three@0.139.0/examples/jsm/loaders/GLTFLoader.js';
  
